305a1a110f37667f4fdffea7ab8f9e6b746b1179
Rhut Vasavada <rhutvasavada@gmail.com>
2017-07-30 18:04:25 -0500

    moved out middleware to a separate file

--- server.js
+++ server.js
@@ -3,16 +3,7 @@ var app = express();
 
 var PORT = 3000;
 
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		console.log('private route hit');
-		next();
-	},
-	logger: function (req, res, next) {
-		console.log('request: ' + new Date().toString() + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
